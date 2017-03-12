4793edd07b3881f00bb59120ded8747da7c76a02
luhaishan <xiaorenwu@163.com>
2017-03-12 15:41:21 +0800

    仓库忽略文件

--- core/mobile/shop/category.php
+++ core/mobile/shop/category.php
@@ -15,6 +15,12 @@ class Category_EweiShopV2Page extends MobilePage
 
 		$getCategory = pdo_fetchall($sql);
 
+//		$sql = "select id,title,content,productprice,marketprice from ims_ewei_shop_goods where istime=1";
+//
+//		$getIstime = pdo_fetchall($sql);
+//        var_dump($getIstime);
+//		exit;
+
 		include $this->template();
 	}
 
@@ -22,12 +28,44 @@ class Category_EweiShopV2Page extends MobilePage
 
 		$id = $_POST['id'];
 
-        $sql = "select title,salesreal,marketprice,thumb from ims_ewei_shop_goods where pcate=" . $id;
+        $sql = "select id,title,salesreal,marketprice,thumb from ims_ewei_shop_goods where pcate=" . $id;
 
 		$getGoods = pdo_fetchall($sql);
 
 		echo json_encode($getGoods);
 
 	}
+
+	public function getIstime(){
+
+		$sql = "select id,title,content,productprice,marketprice,thumb from ims_ewei_shop_goods where istime=1";
+
+		$getIstime = pdo_fetchall($sql);
+
+		echo json_encode($getIstime);
+
+	}
+
+	public function getIsnew1(){
+
+		$sql = "select id,title,productprice,marketprice,thumb from ims_ewei_shop_goods where isnew=1 limit 2";
+
+		$getIsnew1 = pdo_fetchall($sql);
+
+		echo json_encode($getIsnew1);
+
+	}
+
+	public function getIsnew2(){
+
+		$sql = "select id,title,productprice,marketprice,thumb from ims_ewei_shop_goods where isnew=1 order by id desc limit 2";
+
+		$getIsnew2 = pdo_fetchall($sql);
+
+		echo json_encode($getIsnew2);
+
+	}
+
+
 }
 
