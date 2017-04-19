509bd669b8df73b2dcc6f3fd8756784df956e747
cheny <337464978@qq.com>
2017-04-19 16:06:34 +0800

    库存显示

--- core/mobile/goods/indexaa.php
+++ core/mobile/goods/indexaa.php
@@ -27,6 +27,8 @@ class Index_EweiShopV2Page extends MobilePage
 		}
 		include $this->template();
 	}
+	
+	
 	public function gift() 
 	{
 		global $_W;
@@ -75,7 +77,7 @@ class Index_EweiShopV2Page extends MobilePage
 
 		$this->_condition($args);
 	}
-	private function _condition($args)
+	private function _condition($args) 
 	{
 		global $_GPC;
 		global $_W;
@@ -90,15 +92,36 @@ class Index_EweiShopV2Page extends MobilePage
 		{
 			$args['nocommission'] = intval($_GPC['nocommission']);
 		}
+		//库存管理中显示的库存
 		$goods = m('goods')->getList($args);
-
 		foreach ($goods['list'] as $key => $value) {
 			$stock = intval(pdo_fetchcolumn('select stock from ' . tablename('ewei_shop_agent_stock') . ' where goodsid=' . $value['id'] . ' and memberid=' . $member['id'] . ' and optionid=0'));
 			$goods['list'][$key]['stock'] = $stock;
-			
 		}
-		
+        foreach ($goods['list'] as $k =>$v){
+        	$agenttotal = pdo_fetchcolumn('select sum(vstock) from ' . tablename('ewei_shop_agent_stock') . ' where goodsid=' . $v['id'] . ' and optionid=0');
+            $goods[$k]['total'] = intval($goods[$k]['total']) + intval($agenttotal);
+        }
+        	
+        //return $goods['list'];
 		show_json(1, array('list' => $goods['list'], 'total' => $goods['total'], 'pagesize' => $args['pagesize']));
 	}
+/*	//kuu
+	public function getGoods(){
+		
+		$id = $_POST['id'];
+
+        $sql = "select id,title,salesreal,productprice,marketprice,total,thumb from ims_ewei_shop_goods where pcate=" . $id;
+
+		$getGoods = pdo_fetchall($sql);
+//var_dump($getGoods);exit;
+        // 商品库存重新计算
+        foreach($getGoods as $k => $v) {
+            $agenttotal = pdo_fetchcolumn('select sum(vstock) from ' . tablename('ewei_shop_agent_stock') . ' where goodsid=' . $v['id'] . ' and optionid=0');
+            $getGoods[$k]['total'] = intval($getGoods[$k]['total']) + intval($agenttotal);
+        }
+
+		//echo json_encode($getGoods);
+	}*/
 }
 ?>
